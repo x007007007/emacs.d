f0f8feddfec02b40a207a30bae6f3e7c23639c70
xuxingci <xuxingci@ata.net.cn>
2015-07-17 22:41:49 +0800

    add anaconda but have bug

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,9 +5,23 @@
 
 (require-package 'pip-requirements)
 (require-package 'jedi)
+(require-package 'sphinx-doc)
+(require-package 'pyenv-mode)
+(require-package 'anaconda-mode)
 
-(add-hook 'python-mode-hook 'jedi:setup)
+;;;(add-hook 'python-mode-hook 'jedi:setup)
+(setq python-shell-virtualenv-path "~/.emacs.d/.python-environments/default")
+(pyenv-mode)
+
+(add-hook 'python-mode-hook (lambda ()
+                              (require 'sphinx-doc)
+                              (sphinx-doc-mode t)))
+(add-to-list 'company-backends 'company-anaconda)
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'eldoc-mode)
+
 (setq jedi:complete-on-dot t)
-(setq jedi:server-args '("--log-traceback")
-      
+;;;(setq jedi:server-args '("--log-traceback")
+
+
 (provide 'init-python-mode)
